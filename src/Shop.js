9db5356a59c50c3d626bc6323ebb081ffa576bd6
Omer Sevik <omer.sevik@hotmail.fr>
2023-12-28 22:09:20 +0100

    Create Shop.js

---
+++ src/Shop.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Nav from './components/Nav';
+import Products from './components/Products';
+import Footer from './components/Footer';
+
+function Shop() {
+  return (
+    <div>
+        <Nav />
+        <Products />
+        <Footer />
+    </div>
+  )
+}
+
+export default Shop
