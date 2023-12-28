2c75fb0e6580383f71dc9987d23b9935744ffd81
Omer Sevik <omer.sevik@hotmail.fr>
2023-12-28 22:08:11 +0100

    Create ModalButton.js

---
+++ src/components/ModalButton.js
@@ -0,0 +1,12 @@
+import React, {useState} from 'react'
+import {createPortal} from "react-dom" 
+import ModalContent from './ModalContent'
+
+
+function ModalButton() {
+  return (
+    <button><img src='/images/shopping.svg' alt='shopping'/></button>
+  )
+}
+
+export default ModalButton
