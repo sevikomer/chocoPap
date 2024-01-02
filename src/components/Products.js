2d2f7334285ae288a62fff21dabb99be0069a038
Omer Sevik <omer.sevik@hotmail.fr>
2024-01-02 17:54:16 +0100

    Update Products.js

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,23 +1,32 @@
 import React from 'react'
-import ProductsList from '../products.json'
+import datas from '../products.json'
+import Filter from './Filter'
 
 
-function Products() {
-  return (
-    ProductsList.map( productlist => { 
+function Products(props) {
+  const listProducts = datas.map( productlist => { 
       return(
-        <ul key={productlist.id}> 
-          <li>
-            <img src={productlist.image} alt='' />
+        <div key={productlist.id} className='p-20 text-center'>
+            <img src={productlist.image} alt='' className='w-60 h-40'/>
             <p>{productlist.title}</p>
             <p>{productlist.price} €</p>
-            <p>Note : {productlist.note}</p>
-          </li>  
-        </ul> 
+            <p>Note : {productlist.note}</p> 
+            <button className='bg-yellow m-2 p-2 text-white rounded'>Ajouter au panier</button>
+        </div> 
         )
     }
     )
-  )
+    return (
+      <>
+      <h1 className='text-6xl text-center'>BOUTIQUE</h1>
+      <div className='lg:flex sm:block'>
+        <div>
+        <Filter />
+        </div>
+      <ul className='grid lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1'>{listProducts}</ul>
+      </div>
+      </>
+    )
 }
 
 export default Products
