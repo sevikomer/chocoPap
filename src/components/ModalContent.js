877edd7ad99d344c458c6b39ae4adfc101379697
Omer Sevik <omer.sevik@hotmail.fr>
2024-01-02 17:54:05 +0100

    Update ModalContent.js

--- src/components/ModalContent.js
+++ src/components/ModalContent.js
@@ -1,8 +1,21 @@
 import React from 'react'
 
-function ModalContent() {
+function ModalContent({closeModal}) {
   return (
-    <div>ModalContent</div>
+    <>
+        <div onClick={closeModal}
+            className='fixed inset-0 bg-slate-800/75'>
+        </div>
+        <div className='fixed z-10 top-1/2 right-1 -translate-y-1/2 -translate-x-1/2 bg-slate-300 text-slate-900 p-10 rounded'>
+            <button 
+            className='absolute top-1 left-1 w-7 h-7 bg-red-600 text-slate-100 rounded'
+            onClick={closeModal}
+            >
+                X
+            </button>
+            <h1>PANIER</h1>
+        </div>
+    </>
   )
 }
 
