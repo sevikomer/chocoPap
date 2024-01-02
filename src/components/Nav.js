a6f4a0020db94cc708c590046ce2bad505dd4f71
Omer Sevik <omer.sevik@hotmail.fr>
2024-01-02 17:54:11 +0100

    Update Nav.js

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,16 +1,15 @@
 import React from 'react'
-import "./Nav.css"
 import { Link } from 'react-router-dom';
 import ModalButton from './ModalButton';
 
 function Nav() {
   return (
-    <div className='flex'>
-        <div className='m-1'>
-            <img src={'/images/logo.png'} alt="logo" className='w-20'/>
+    <div className='flex justify-between bg-orange'>
+        <div className='m-1 p-3'>
+            <Link to="/" className=''><img src={'/images/logo.png'} alt="logo" className='w-20'/></Link>
         </div>
-        <nav className='m-1'>
-           <Link to="/">Accueil</Link>
+        <nav className='m-1 p-4 flex items-center gap-8 text-xl'>
+           <Link to="/" className=''>Accueil</Link>
            <Link to="/shop">Boutique</Link>
            <ModalButton />
         </nav>
