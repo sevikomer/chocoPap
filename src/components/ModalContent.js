b387e5bd227618bf53b98cecf04a4b4f6e05444d
Omer Sevik <omer.sevik@hotmail.fr>
2024-01-05 14:29:51 +0100

    Update ModalContent.js

--- src/components/ModalContent.js
+++ src/components/ModalContent.js
@@ -6,7 +6,7 @@ function ModalContent({closeModal}) {
         <div onClick={closeModal}
             className='fixed inset-0 bg-slate-800/75'>
         </div>
-        <div className='fixed z-10 top-1/2 right-1 -translate-y-1/2 -translate-x-1/2 bg-slate-300 text-slate-900 p-10 rounded'>
+        <div className='fixed z-10 top-1/2 right-1 -translate-y-1/2 -translate-x-1/2 bg-white text-slate-900 p-10 rounded'>
             <button 
             className='absolute top-1 left-1 w-7 h-7 bg-red-600 text-slate-100 rounded'
             onClick={closeModal}
@@ -14,6 +14,9 @@ function ModalContent({closeModal}) {
                 X
             </button>
             <h1>PANIER</h1>
+            <ul>
+              
+            </ul>
         </div>
     </>
   )
