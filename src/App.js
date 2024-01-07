ec52985b1fccf2f46f50fff362b0ed9eac68c987
Omer Sevik <omer.sevik@hotmail.fr>
2024-01-07 23:35:39 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,13 +6,34 @@ import { BrowserRouter } from 'react-router-dom';
 import data from './products.json'
 
 function App() {
- const {productItems} = data;
- const [cartItems, setCartItems] = useState(0)
+ const productItems = data;
+ const [cartItems, setCartItems] = useState([])
+ const handleAddProduct = (productItem) => { 
+  const ProductExist = cartItems.find((cartItem) => cartItem.id === productItem.id);
+  if(ProductExist){
+    setCartItems(cartItems.map((cartItem) => cartItem.id === productItem.id ? {...ProductExist, quantity: ProductExist.quantity + 1}:cartItem));
+  }
+  else {
+    setCartItems([...cartItems, {...productItem, quantity:1}])
+  }
+} 
+const handleRemoveProduct = (productItem) => {
+  const ProductExist = cartItems.find((cartItem) => cartItem.id === productItem.id);
+  if(ProductExist.quantity === 1){
+    setCartItems(cartItems.filter((cartItem) => cartItem.id !== productItem.id));
+  }
+  else {
+    setCartItems(cartItems.map((cartItem) => cartItem.id === productItem.id ? {...ProductExist, quantity: ProductExist.quantity - 1}:cartItem));
+  }
+}
+const handleCartClearance = () => {
+  setCartItems([])
+}
   return (
     <div>
       <BrowserRouter>
-        <Header productItems={productItems} cartItems={cartItems} setCartItems={setCartItems}/>
-        <Nav productItems={productItems} cartItems={cartItems} setCartItems={setCartItems}/>
+        <Header productItems={productItems} cartItems={cartItems} setCartItems={setCartItems} handleAddProduct={handleAddProduct} handleRemoveProduct={handleRemoveProduct} handleCartClearance={handleCartClearance}/>
+        <Nav productItems={productItems} cartItems={cartItems} setCartItems={setCartItems} handleAddProduct={handleAddProduct} handleRemoveProduct={handleRemoveProduct} handleCartClearance={handleCartClearance}/>
         <Footer />
       </BrowserRouter>
     </div>
