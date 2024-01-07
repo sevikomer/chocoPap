0c7d0dd18cc31d749628c472b9843b6bb62f44ca
Omer Sevik <omer.sevik@hotmail.fr>
2024-01-07 23:35:55 +0100

    Update Nav.js

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,15 +1,17 @@
 import React from 'react'
 import {Routes, Route} from "react-router-dom"
 import Home from './Home'
-import Products from './Products'
+import Shop from './Shop'
+import ModalContent from './ModalContent'
 
 
-function Nav({productItems}) {
+function Nav({productItems, cartItems, setCartItems, handleAddProduct, handleRemoveProduct, handleCartClearance}) {
   return (
     <div>
         <Routes>
           <Route path="/" Component={Home} />
-          <Route path="/shop" Component={Products} productItems={productItems}/>
+          <Route path="/shop" element={<Shop productItems={productItems} cartItems={cartItems} setCartItems={setCartItems} handleAddProduct={handleAddProduct} handleRemoveProduct={handleRemoveProduct} handleCartClearance={handleCartClearance}/>} />
+          <Route path="/cart" element={<ModalContent productItems={productItems} cartItems={cartItems} setCartItems={setCartItems} handleAddProduct={handleAddProduct} handleRemoveProduct={handleRemoveProduct} handleCartClearance={handleCartClearance}/>} />
         </Routes>
     </div>
   )
