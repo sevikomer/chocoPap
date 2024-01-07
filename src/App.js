72c99a7f17c6db4eede0467af79a095b37608726
Omer Sevik <omer.sevik@hotmail.fr>
2024-01-07 13:59:34 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
-import Slider from './components/Slider';
-import Nav from './components/Nav';
+import React, { useState } from 'react';
+import Header from './components/Header';
 import Footer from './components/Footer';
-
+import Nav from './components/Nav';
+import { BrowserRouter } from 'react-router-dom';
+import data from './products.json'
 
 function App() {
-
+ const {productItems} = data;
+ const [cartItems, setCartItems] = useState(0)
   return (
-    <div className="App">
-      <Nav />
-      <Slider />
-      <Footer />
+    <div>
+      <BrowserRouter>
+        <Header productItems={productItems} cartItems={cartItems} setCartItems={setCartItems}/>
+        <Nav productItems={productItems} cartItems={cartItems} setCartItems={setCartItems}/>
+        <Footer />
+      </BrowserRouter>
     </div>
   );
 }
