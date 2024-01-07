2749482255f45b12a1e73a5f2361cbda6eebc0b6
Omer Sevik <omer.sevik@hotmail.fr>
2024-01-07 23:35:59 +0100

    Create Shop.js

---
+++ src/components/Shop.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import Filter from './Filter'
+
+function Shop({productItems, cartItems, setCartItems, handleAddProduct, handleRemoveProduct, handleCartClearance}) {
+  const Products = productItems?.map( productItem => { 
+      return(
+        <div key={productItem.id} className='p-20 text-center'>
+            <div>
+              <img src={productItem.image} alt='' className='w-60 h-40'/>
+            </div>
+            <div>
+              <h3>{productItem.title}</h3>
+            </div>
+            <div>{productItem.price} €</div>
+            <div>Note : {productItem.note}</div> 
+            <div>
+              <button className='bg-yellow m-2 p-2 text-white rounded' onClick={() => handleAddProduct(productItem)}>Ajouter au panier</button>
+            </div>
+        </div> 
+        )
+    }
+    )
+    return (
+      <>
+      <h1 className='text-6xl text-center'>BOUTIQUE</h1>
+      <div className='lg:flex sm:block'>
+        <div>
+        <Filter />
+        </div>
+      <ul className='grid lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1'>{Products}</ul>
+      </div>
+      </>
+    )
+}
+
+export default Shop
