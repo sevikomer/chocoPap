8d6cb1beea16f4ce4b5e22b1a76d9063817c8c99
Omer Sevik <omer.sevik@hotmail.fr>
2024-01-08 14:51:28 +0100

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { Link } from 'react-router-dom';
 import ModalButton from './ModalButton';
 
-function Header({productItems, cartItems, setCartItems, handleAddProduct, handleRemoveProduct, handleCartClearance}) {
+function Header({productItems, cartItems, setCartItems, handleAddProduct, handleRemoveProduct, handleCartClearance, deleteProduct}) {
   return (
     <div className='flex justify-between bg-orange text-white'>
         <div className='m-1 p-3'>
@@ -11,7 +11,7 @@ function Header({productItems, cartItems, setCartItems, handleAddProduct, handle
         <nav className='m-1 p-4 flex items-center gap-8 text-xl'>
            <Link to="/" className=''>Accueil</Link>
            <Link to="/shop">Boutique</Link>
-           <ModalButton productItems={productItems} cartItems={cartItems} setCartItems={setCartItems} handleAddProduct={handleAddProduct} handleRemoveProduct={handleRemoveProduct} handleCartClearance={handleCartClearance}/>
+           <ModalButton productItems={productItems} cartItems={cartItems} setCartItems={setCartItems} handleAddProduct={handleAddProduct} handleRemoveProduct={handleRemoveProduct} handleCartClearance={handleCartClearance} deleteProduct={deleteProduct}/>
         </nav>
     </div>
   )
